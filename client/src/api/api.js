07f56a39af5c2159a2d68a95f055bb0fb847f9cd
Nitya-nama <nityanama101@gmail.com>
2026-03-29 15:52:45 +0530

    render link change

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: window.location.hostname === "localhost"
     ? "http://127.0.0.1:5000/api"
-    : "https://cinerent-erp.onrender.com/api",
+    : "https://cinerent-erp-d2fs.onrender.com/",
 });
 
 // ✅ Attach JWT token to every request
